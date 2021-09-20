fd80af3ca95208ee15e6bae885364250bc5a42be
sariego <pedro@sariego.dev>
2021-09-20 13:56:36 -0300

    refactor compose grid into columns and rows

--- app/src/main/java/dev/sariego/liniofavorites/app/favorites/presentation/screens/FavoritesScreen.kt
+++ app/src/main/java/dev/sariego/liniofavorites/app/favorites/presentation/screens/FavoritesScreen.kt
@@ -1,48 +1,78 @@
 package dev.sariego.liniofavorites.app.favorites.presentation.screens
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.sariego.liniofavorites.R
 import dev.sariego.liniofavorites.app.favorites.presentation.state.FavoritesScreenState.*
 import dev.sariego.liniofavorites.app.favorites.presentation.state.FavoritesViewModel
 
-@ExperimentalFoundationApi
 @Composable
 fun FavoritesScreen(
     favoritesViewModel: FavoritesViewModel = viewModel()
 ) {
     val state = favoritesViewModel.states.collectAsState()
     when (state.value) {
-        Loading -> CircularProgressIndicator()
+        Loading -> Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            CircularProgressIndicator()
+        }
         is DisplayingError -> {
             val message = (state.value as DisplayingError).throwable.message
-            Text(message.orEmpty())
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(message.orEmpty())
+            }
         }
         is DisplayingFavorites -> {
             val data = state.value as DisplayingFavorites
-            LazyVerticalGrid(
-                cells = GridCells.Fixed(2),
-            ) {
-                items(data.collections) {
-
-                }
-                item {
-                    val header = "${stringResource(R.string.my_favorites_header)} (${data.products.size})"
-                    Text(header)
-                }
-                items(data.products) {
-                    
-                }
-            }
-
+            FavoritesInnerScreen(data)
         }
     }
 }
+
+@Composable
+private fun FavoritesInnerScreen(data: DisplayingFavorites) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(data.collections.chunked(NUM_ROWS)) { collections ->
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                collections.forEach { Text(it.name, Modifier.weight(weight = 1F)) }
+            }
+        }
+        item {
+            val header = "${stringResource(R.string.my_favorites_header)} (${data.products.size})"
+            Box(
+                modifier = Modifier
+                .height(77.dp)
+                .fillMaxWidth(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(header)
+            }
+
+        }
+        items(data.products.chunked(NUM_ROWS)) { products ->
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+               products.forEach { Text(it.name, Modifier.weight(weight = 1F)) }
+            }
+        }
+    }
+}
+
+private const val NUM_ROWS = 2
