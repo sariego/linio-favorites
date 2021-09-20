3724827371e71ff744946539c92c71430d6dd2b2
sariego <pedro@sariego.dev>
2021-09-20 18:11:04 -0300

    update app bar with correct styles

--- app/src/main/java/dev/sariego/liniofavorites/core/entrypoint/MainActivity.kt
+++ app/src/main/java/dev/sariego/liniofavorites/core/entrypoint/MainActivity.kt
@@ -3,16 +3,24 @@ package dev.sariego.liniofavorites.core.entrypoint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import com.example.myapplication.ui.theme.FavoritesAppTheme
+import coil.compose.rememberImagePainter
 import dagger.hilt.android.AndroidEntryPoint
 import dev.sariego.liniofavorites.R
 import dev.sariego.liniofavorites.app.favorites.presentation.screens.FavoritesScreen
+import dev.sariego.liniofavorites.core.ui.theme.AppTitle
+import dev.sariego.liniofavorites.core.ui.theme.FavoritesAppTheme
+import dev.sariego.liniofavorites.core.ui.theme.FavoritesBackground
 
-
-@ExperimentalFoundationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
@@ -20,12 +28,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FavoritesAppTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Scaffold(
-                        topBar = { TopAppBar { Text(stringResource(R.string.my_favorites_title)) } }
-                    ) {
-                        FavoritesScreen()
-                    }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Box(Modifier.fillMaxWidth()) {
+                                    Text(
+                                        text = stringResource(R.string.my_favorites_title),
+                                        style = AppTitle,
+                                        modifier = Modifier.align(Alignment.Center)
+                                    )
+                                }
+                            },
+                            actions = {
+                                      Icon(
+                                          painter = rememberImagePainter(R.drawable.nd_ic_add_collection_32),
+                                          contentDescription = null,
+                                          modifier = Modifier.clickable {  }
+                                      ) // this action does nothing
+                            },
+                            backgroundColor = FavoritesBackground
+                        )
+                    },
+                    backgroundColor = FavoritesBackground
+                ) {
+                    FavoritesScreen()
                 }
             }
         }
